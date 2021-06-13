fb17d186ba122849ba368465c17473b69ef9a76f
Colin Chauche <colin.chauche@gmail.com>
2021-06-13 19:34:09 -0400

    Add custom theme and modify colors to match radiatorlabs

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { CssBaseline, makeStyles } from '@material-ui/core';
+import { CssBaseline } from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
 import { DRAWER_WIDTH, HEADER_HEIGHT } from './config/layoutConfig';
 import { fetch, BuildingParser, config } from '../utils';
 import Header from './Header/Header';
--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import App from './Components/App';
-// import '@fontsource/roboto';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#ffff6b',
+      main: '#fdd835',
+      dark: '#c6a700',
+    },
+    secondary: {
+      light: '#80e27e',
+      main: '#4caf50',
+      dark: '#087f23'
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
+
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById('app')
+);
