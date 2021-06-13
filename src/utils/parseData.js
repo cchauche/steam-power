6304a64b309f85dd4595356d3458d8960a10b5d5
Colin Chauche <colin.chauche@gmail.com>
2021-06-13 12:03:05 -0400

    Refactor building parser to a class

--- src/utils/parseData.js
+++ src/utils/parseData.js
@@ -1,39 +1,71 @@
 import config from './config';
 
-const decorateFloorErr = (floor) => {
-  let errors = {
-    tempErr: [],
-    unresponsive: [],
-  };
-  // Parse the common area
-  // Parse each unit
-  floor.errors = errors;
-  return floor;
-};
+class BuildingParser {
+  constructor(building) {
+    this.floors = building.floors;
+    this.retrievalTime = building.retrieved_at;
+    this.unresponsiveCount = 0;
+    this.tempErrCount = 0;
+  }
 
-const checkSpaces = (spaces, errors) => {
-  // For each space check all the radiators
-};
-
-const checkRadiator = (radiator, errors) => {};
-
-const checkRadiatorTemp = (node) => {};
-
-const checkIfUnresponsive = (node) => {};
-
-/**
- * Will decorate the node with a 'room_feel' property
- * @param {Object} node Radiator node object
- * @return {Object} Radiator node object
- */
-const decorateRoomFeel = (node) => {
-  let temp = node.room_temperature;
-  for (const el of config.ROOM_TEMP_SCALE) {
-    if (temp < el.high) {
-      node.room_feel = el.name;
-      return node;
+  parseBuilding() {
+    for (const floor of this.floors) {
+      this.decorateFloorErr(floor);
     }
   }
-};
 
-export default { decorateFloorErr };
+  decorateFloorErr(floor) {
+    const errors = {
+      tempErr: [],
+      unresponsive: [],
+    };
+    // Parse the common area
+    // Parse each unit
+    floor.errors = errors;
+    return floor;
+  }
+
+  checkSpaces(spaces, errors) {
+    // For each space check all the radiators
+  }
+
+  checkRadiator(radiator) {}
+
+  checkRadiatorTemp(node) {
+    let radiatorTemp = node.radiator_temperature;
+    let roomTemp = node.room_temperature;
+    if (Math.abs(radiatorTemp - roomTemp) <= config.RADIATOR_TEMP_RANGE) {
+      node.hasTempErr = true;
+      return true;
+    }
+    node.hasTempErr = false;
+    return false;
+  }
+
+  checkIfUnresponsive(node) {
+    let messageTime = node.last_message;
+    if (this.retrievalTime - messageTime >= config.RESPONSE_INTERVAL) {
+      node.isUnresponsive = true;
+      return true;
+    }
+    node.isUnresponsive = false;
+    return false;
+  }
+
+  /**
+   * Will decorate the node with a 'room_feel' property
+   * @param {Object} node Radiator node object
+   * @return {Object} Radiator node object
+   */
+  decorateRoomFeel(node) {
+    let temp = node.room_temperature;
+    for (const el of config.ROOM_TEMP_SCALE) {
+      if (temp < el.high) {
+        node.room_feel = el.name;
+        return node;
+      }
+    }
+  }
+}
+
+export default { BuildingParser };
