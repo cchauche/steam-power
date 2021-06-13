aa09cd017b83d6449c3ee074cd9e4febce03b570
Colin Chauche <colin.chauche@gmail.com>
2021-06-13 10:41:37 -0400

    Add constants to determine errors

---
+++ src/utils/config.js
@@ -0,0 +1,10 @@
+export default {
+  RESPONSE_INTERVAL: 600000, // 10 Minutes in milliseconds
+  RADIATOR_TEMP_RANGE: 50, // Radiator is bad if withing 50 deg of room
+  ROOM_TEMP_SCALE: [
+    {name: 'Cold', high:70},
+    {name: 'Cozy', high: 75},
+    {name: 'Rather Warm', high: 80},
+    {name: 'Hot', high: null}
+  ],
+};
