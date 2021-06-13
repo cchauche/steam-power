161a7e158bf6cc54536675ba4f45d356a9f26fd6
Colin Chauche <colin.chauche@gmail.com>
2021-06-13 19:49:46 -0400

    Add warning badges with error count

--- src/Components/Sidebar/FloorMenuItem.jsx
+++ src/Components/Sidebar/FloorMenuItem.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ListItem, ListItemText } from '@material-ui/core';
+import { ListItem, ListItemText, Badge } from '@material-ui/core';
+import WarningIcon from '@material-ui/icons/Warning';
 import { toFriendlyFloorName } from '../../utils';
 
 function FloorMenuItem({ floor, index, selectedFloor, setSelectedFloor }) {
   let totalErrors =
     floor.errors.tempErr.length + floor.errors.unresponsive.length;
   return (
-    <ListItem
-      onClick={() => {
-        console.log(
-          `Clicked on floor: ${index} which has ${totalErrors} errors.`
-        );
-        setSelectedFloor(index);
-      }}
-    >
+    <ListItem onClick={() => setSelectedFloor(index)}>
       <ListItemText primary={toFriendlyFloorName(floor.name)} />
+      {totalErrors > 0 ? (
+        <Badge
+          badgeContent={totalErrors}
+          color="error"
+          overlap="circle"
+          max={9}
+        >
+          <WarningIcon />
+        </Badge>
+      ) : null}
     </ListItem>
   );
 }
