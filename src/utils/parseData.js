00fa731ba32e6602d0e5a5cac327500b3b26049a
Colin Chauche <colin.chauche@gmail.com>
2021-06-13 11:08:58 -0400

    Implement decorateRoomFeel function

--- src/utils/parseData.js
+++ src/utils/parseData.js
@@ -1,7 +1,9 @@
+import config from './config';
+
 const decorateFloorErr = (floor) => {
   let errors = {
     tempErr: [],
-    unresponsive: []
+    unresponsive: [],
   };
   // Parse the common area
   // Parse each unit
@@ -10,20 +12,28 @@ const decorateFloorErr = (floor) => {
 };
 
 const checkSpaces = (spaces, errors) => {
-
+  // For each space check all the radiators
 };
 
-const checkRadiatorTemp = (node) => {
+const checkRadiator = (radiator, errors) => {};
 
+const checkRadiatorTemp = (node) => {};
+
+const checkIfUnresponsive = (node) => {};
+
+/**
+ * Will decorate the node with a 'room_feel' property
+ * @param {Object} node Radiator node object
+ * @return {Object} Radiator node object
+ */
+const decorateRoomFeel = (node) => {
+  let temp = node.room_temperature;
+  for (const el of config.ROOM_TEMP_SCALE) {
+    if (temp < el.high) {
+      node.room_feel = el.name;
+      return node;
+    }
+  }
 };
 
-const checkIfUnresponsive = (node) => {
-
-};
-
-const checkRoomTemp = (node) => {
-
-};
-
-
-export default {decorateFloorErr};
+export default { decorateFloorErr };
